122e0b33d1c7d69de13e24c9dd99743627c82ec5
MMello <matheusnarciso@hotmail.com>
2018-03-14 07:30:41 -0300

    removendo log

--- application/logs/log-2018-03-07.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2018-03-07 23:43:24 --> 404 Page Not Found: /index
-ERROR - 2018-03-07 23:52:03 --> 404 Page Not Found: /index
-ERROR - 2018-03-07 23:52:18 --> 404 Page Not Found: Forgot-password/index
-ERROR - 2018-03-07 23:52:28 --> 404 Page Not Found: /index
